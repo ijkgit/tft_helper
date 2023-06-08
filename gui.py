9bcebc40adb36669df8ccaf3190cf82dd8152057
Jay <dlaworud@koreatech.ac.kr>
2023-06-08 16:54:10 +0900

    v 1.1.0 : 자동 리롤 선택 기능 추가, 이미지 정확도 0.6 set, 챔피언 추가

--- gui.py
+++ gui.py
@@ -6,7 +6,10 @@ def on_start_button_click():
     for checkbox_value, item in zip(checkbox_values, items):
         if checkbox_value.get():
             selected_items.append(item)
-    result = main.start_button_click(selected_items)
+    main.start_button_click(selected_items)
+
+def isAuto():
+    main.isAuto(auto_value.get())
 
 # 윈도우 생성
 window = tk.Tk()
@@ -14,7 +17,7 @@ window.geometry("500x500")
 
 # 아이템 추가
 checkbox_values = []
-items = ["lulu", "mal", "pan", "gank", "lee", "poppy", "ash", "blitz"]
+items = ["lulu", "mal", "pan", "gank", "lee", "poppy", "ash", "blitz", "lucian", "vi"]
 for item in items:
     checkbox_value = tk.BooleanVar()
     checkbox = tk.Checkbutton(window, text=item, variable=checkbox_value)
@@ -24,6 +27,12 @@ for item in items:
 # 시작 버튼 생성
 start_button = tk.Button(window, text="Start", command=on_start_button_click)
 start_button.pack()
+
+# 오토 리롤
+auto_value = tk.BooleanVar()
+checkbox = tk.Checkbutton(window, text="auto d", variable=auto_value)
+checkbox.pack()
+checkbox_values.append(auto_value)
 
 # 윈도우 실행
 window.mainloop()
--- main.py
+++ main.py
@@ -8,34 +8,39 @@ def start_button_click(selected_item):
     print("Start button clicked with selected item:", selected_item)
     find_character(selected_item)
 
+def isAuto(auto_value):
+    global auto
+    auto = auto_value
+
 def find_character(selected_item):
     while True:
         for s in selected_item:
-            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(413, 900, 130, 25))
+            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(413, 900, 130, 25), confidence=0.6)
             if find != None:
                 print(find)
                 pyautogui.click(find[0]+100, find[1])
                 pyautogui.mouseUp(button='left')
-            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(576, 900, 130, 25))
+            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(576, 900, 130, 25), confidence=0.6)
             if find != None:
                 print(find)
                 pyautogui.click(find[0]+100, find[1])
                 pyautogui.mouseUp(button='left')
-            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(743, 900, 130, 25))
+            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(743, 900, 130, 25), confidence=0.6)
             if find != None:
                 print(find)
                 pyautogui.click(find[0]+100, find[1])
                 pyautogui.mouseUp(button='left')
-            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(912, 900, 130, 25))
+            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(912, 900, 130, 25), confidence=0.6)
             if find != None:
                 print(find)
                 pyautogui.click(find[0]+100, find[1])
                 pyautogui.mouseUp(button='left')
-            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(1082, 900, 130, 25))
+            find = pyautogui.locateCenterOnScreen("./img/"+s+".png", region=(1082, 900, 130, 25), confidence=0.6)
             if find != None:
                 print(find)
                 pyautogui.click(find[0]+100, find[1])
                 pyautogui.mouseUp(button='left')
         else:
-            pydirectinput.keyDown('d')
-            pydirectinput.keyUp('d')
+            if auto:
+                pydirectinput.keyDown('d')
+                pydirectinput.keyUp('d')
